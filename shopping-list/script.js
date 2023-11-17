906e18052127d97e347cc94b3c850f0f6b3d263d
pouryourlove <lqu134@naver.com>
2023-11-17 18:12:28 +0900

    code with the lecture

--- shopping-list/script.js
+++ shopping-list/script.js
@@ -1,20 +1,62 @@
-const form = document.getElementById('item-form')
-const addBtn = document.querySelector('.btn')
-const ul = document.getElementById('item-list')
+// with the lecture
+const itemForm = document.getElementById("item-form");
+const itemInput = document.getElementById("item-input");
+const itemList = document.getElementById("item-list");
 
+function addItem(e) {
+  e.preventDefault();
 
-function onAdd(e){
-    e.preventDefault()
-    const inputValue = form.querySelector('.form-input').value;
-    const li = document.createElement('li')
-    li.innerHTML = `
-        ${inputValue}
-        <button class="remove-item btn-link text-red">
-            <i class="fa-solid fa-xmark"></i>
-        </button>` 
+  const newItem = itemInput.value;
 
-    ul.appendChild(li)
+  //Validate Input
+  if (newItem === "") {
+    alert("Please add an item");
+    return;
+  }
+  // Create list item
+  const li = document.createElement("li");
+  li.appendChild(document.createTextNode(newItem));
+
+  const button = createButton("remove-item btn-link text-red");
+  li.appendChild(button);
+
+  itemList.appendChild(li);
+
+  itemInput.value = "";
 }
 
+function createButton(classes) {
+  const button = document.createElement("button");
+  button.className = classes;
+  const icon = createIcon("fa-solid fa-xmark");
+  button.appendChild(icon);
+  return button;
+}
 
-addBtn.addEventListener('click',onAdd)
+function createIcon(classes) {
+  const icon = document.createElement("i");
+  icon.className = classes;
+  return icon;
+}
+
+itemForm.addEventListener("submit", addItem);
+
+//first trial
+// const form = document.getElementById('item-form')
+// const addBtn = document.querySelector('.btn')
+// const ul = document.getElementById('item-list')
+
+// function onAdd(e){
+//     e.preventDefault()
+//     const inputValue = form.querySelector('.form-input').value;
+//     const li = document.createElement('li')
+//     li.innerHTML = `
+//         ${inputValue}
+//         <button class="remove-item btn-link text-red">
+//             <i class="fa-solid fa-xmark"></i>
+//         </button>`
+
+//     ul.appendChild(li)
+// }
+
+// addBtn.addEventListener('click',onAdd)
