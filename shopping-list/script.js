b54bcbedb93a8c007b9c9caa6f28e76b97173157
Juyeon Lee <lqu134@naver.com>
2023-11-20 14:22:56 +0900

    remove & clear items

--- shopping-list/script.js
+++ shopping-list/script.js
@@ -1,7 +1,8 @@
-// with the lecture
+
 const itemForm = document.getElementById("item-form");
 const itemInput = document.getElementById("item-input");
 const itemList = document.getElementById("item-list");
+const ClearBtn = document.getElementById('clear')
 
 function addItem(e) {
   e.preventDefault();
@@ -39,7 +40,64 @@ function createIcon(classes) {
   return icon;
 }
 
+function removeItem(e){
+    if(e.target.parentElement.classList.contains("remove-item")){
+        e.target.parentElement.parentElement.remove()
+    }
+}
+
+function clearItems(){
+    while(itemList.firstChild){
+        itemList.removeChild(itemList.firstChild)
+    }
+}
+
 itemForm.addEventListener("submit", addItem);
+itemList.addEventListener("click",removeItem);
+ClearBtn.addEventListener("click",clearItems)
+
+// // with the lecture
+// const itemForm = document.getElementById("item-form");
+// const itemInput = document.getElementById("item-input");
+// const itemList = document.getElementById("item-list");
+
+// function addItem(e) {
+//   e.preventDefault();
+
+//   const newItem = itemInput.value;
+
+//   //Validate Input
+//   if (newItem === "") {
+//     alert("Please add an item");
+//     return;
+//   }
+//   // Create list item
+//   const li = document.createElement("li");
+//   li.appendChild(document.createTextNode(newItem));
+
+//   const button = createButton("remove-item btn-link text-red");
+//   li.appendChild(button);
+
+//   itemList.appendChild(li);
+
+//   itemInput.value = "";
+// }
+
+// function createButton(classes) {
+//   const button = document.createElement("button");
+//   button.className = classes;
+//   const icon = createIcon("fa-solid fa-xmark");
+//   button.appendChild(icon);
+//   return button;
+// }
+
+// function createIcon(classes) {
+//   const icon = document.createElement("i");
+//   icon.className = classes;
+//   return icon;
+// }
+
+// itemForm.addEventListener("submit", addItem);
 
 //first trial
 // const form = document.getElementById('item-form')
