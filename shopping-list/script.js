e7bf9edcdfee346c01840a580bad7907c29539a4
Juyeon Lee <lqu134@naver.com>
2023-11-20 16:04:01 +0900

    checkUI added

--- shopping-list/script.js
+++ shopping-list/script.js
@@ -1,8 +1,9 @@
-
 const itemForm = document.getElementById("item-form");
 const itemInput = document.getElementById("item-input");
 const itemList = document.getElementById("item-list");
-const ClearBtn = document.getElementById('clear')
+const clearBtn = document.getElementById("clear")
+const itemFilter = document.getElementById("filter")
+
 
 function addItem(e) {
   e.preventDefault();
@@ -23,7 +24,11 @@ function addItem(e) {
 
   itemList.appendChild(li);
 
+  checkUI()
+
   itemInput.value = "";
+
+
 }
 
 function createButton(classes) {
@@ -40,21 +45,97 @@ function createIcon(classes) {
   return icon;
 }
 
-function removeItem(e){
-    if(e.target.parentElement.classList.contains("remove-item")){
-        e.target.parentElement.parentElement.remove()
-    }
+function deleteItem(e){
+   if(e.target.parentElement.classList.contains("remove-item")){
+    if(confirm("Are you sure?")){
+        e.target.parentElement.parentElement.remove();
+        checkUI()
+   
+   }
 }
 
-function clearItems(){
-    while(itemList.firstChild){
-        itemList.removeChild(itemList.firstChild)
+function clearAll(){
+   while(itemList.firstChild){
+    itemList.removeChild(itemList.firstChild)
+   }
+   checkUI()
+}
+
+function checkUI(){
+    //everytime function runs, we are taking the item
+    const items = itemList.querySelectorAll("li")
+    if(items.length === 0){
+        clearBtn.style.display = "none"
+        itemFilter.style.display = "none"
+    }else{
+        clearBtn.style.display = "block"
+        itemFilter.style.display = "block"
     }
 }
 
 itemForm.addEventListener("submit", addItem);
-itemList.addEventListener("click",removeItem);
-ClearBtn.addEventListener("click",clearItems)
+itemList.addEventListener("click",deleteItem)
+clearBtn.addEventListener("click",clearAll)
+
+//when page loads
+checkUI()
+
+// const itemForm = document.getElementById("item-form");
+// const itemInput = document.getElementById("item-input");
+// const itemList = document.getElementById("item-list");
+// const ClearBtn = document.getElementById('clear')
+
+// function addItem(e) {
+//   e.preventDefault();
+
+//   const newItem = itemInput.value;
+
+//   //Validate Input
+//   if (newItem === "") {
+//     alert("Please add an item");
+//     return;
+//   }
+//   // Create list item
+//   const li = document.createElement("li");
+//   li.appendChild(document.createTextNode(newItem));
+
+//   const button = createButton("remove-item btn-link text-red");
+//   li.appendChild(button);
+
+//   itemList.appendChild(li);
+
+//   itemInput.value = "";
+// }
+
+// function createButton(classes) {
+//   const button = document.createElement("button");
+//   button.className = classes;
+//   const icon = createIcon("fa-solid fa-xmark");
+//   button.appendChild(icon);
+//   return button;
+// }
+
+// function createIcon(classes) {
+//   const icon = document.createElement("i");
+//   icon.className = classes;
+//   return icon;
+// }
+
+// function removeItem(e){
+//     if(e.target.parentElement.classList.contains("remove-item")){
+//         e.target.parentElement.parentElement.remove()
+//     }
+// }
+
+// function clearItems(){
+//     while(itemList.firstChild){
+//         itemList.removeChild(itemList.firstChild)
+//     }
+// }
+
+// itemForm.addEventListener("submit", addItem);
+// itemList.addEventListener("click",removeItem);
+// ClearBtn.addEventListener("click",clearItems)
 
 // // with the lecture
 // const itemForm = document.getElementById("item-form");
